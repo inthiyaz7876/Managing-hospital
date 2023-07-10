a7f2d407ac0f3ae52b6d7bd2479df5442b19cfe8
inthiyaz7876 <30855037+inthiyaz7876@users.noreply.github.com>
2023-07-10 21:34:54 +0530

    1 file modified

--- src/components/Availablestaff.js
+++ src/components/Availablestaff.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-function AvailableStaff({staffData}) {
+function AvailableStaff({ staffData }) {
   return (
     <ListGroup as="ol" numbered>
-      {staffData.map((item)=> (<ListGroup.Item as="li">{item}</ListGroup.Item>))}
+      {staffData.map((item) => (
+        <ListGroup.Item as="li">{item}</ListGroup.Item>
+      ))}
     </ListGroup>
   );
 }
